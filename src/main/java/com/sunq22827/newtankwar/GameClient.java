91be5294d846a03ef1edf959ca20aee039300246
sunq2018 <sunq22827@gmail.com>
2020-03-17 21:48:14 -0700

    图片声音资源库

---
+++ src/main/java/com/sunq22827/newtankwar/GameClient.java
@@ -0,0 +1,29 @@
+package com.sunq22827.newtankwar;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameClient extends JComponent {
+
+    public GameClient() {
+        this.setPreferredSize(new Dimension(800,600));
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(),
+                400,200,null);
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("DIY好玩无用的小游戏");
+        frame.setIconImage(new ImageIcon("assets/images/icon.png").getImage());
+        GameClient client = new GameClient();
+        frame.add(client);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
