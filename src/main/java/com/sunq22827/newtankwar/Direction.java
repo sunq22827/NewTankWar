8775fb6c0490728b9b903bc92622027ee14a6c41
sunq2018 <sunq22827@gmail.com>
2020-03-17 23:33:45 -0700

    坦克八个方向行走

---
+++ src/main/java/com/sunq22827/newtankwar/Direction.java
@@ -0,0 +1,14 @@
+package com.sunq22827.newtankwar;
+
+public enum  Direction {
+
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT,
+
+    UPLEFT,
+    DOWNLEFT,
+    DOWNRIGHT,
+    UPRIGHT;
+}
--- src/main/java/com/sunq22827/newtankwar/GameClient.java
+++ src/main/java/com/sunq22827/newtankwar/GameClient.java
@@ -2,18 +2,21 @@ package com.sunq22827.newtankwar;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 public class GameClient extends JComponent {
 
+    private Tank playerTank;
     public GameClient() {
+        this.playerTank = new Tank(400,200,Direction.DOWN);
         this.setPreferredSize(new Dimension(800,600));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        g.drawImage(new ImageIcon("assets/images/tankD.gif").getImage(),
-                400,200,null);
+        playerTank.draw(g);
     }
 
 
@@ -21,11 +24,33 @@ public class GameClient extends JComponent {
         JFrame frame = new JFrame();
         frame.setTitle("DIY好玩无用的小游戏");
         frame.setIconImage(new ImageIcon("assets/images/icon.png").getImage());
-        GameClient client = new GameClient();
+        final GameClient client = new GameClient();
         frame.add(client);
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.pack();
+        frame.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+                client.playerTank.keyPressed(e);
+                }
+
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+                client.playerTank.keyReleased(e);
+            }
+        });
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+        while (true){
+            client.repaint();
+            try {
+                Thread.sleep(50);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
+
 }
